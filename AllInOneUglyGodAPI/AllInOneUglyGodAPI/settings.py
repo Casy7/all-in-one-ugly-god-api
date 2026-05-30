af90de153fc3305c4c040715ea1c2ccdd5b08611
alex-zakrzhevskiy <77270084+alex-zakrzhevskiy@users.noreply.github.com>
2026-05-30 20:54:36 +0300

    Added static url

--- AllInOneUglyGodAPI/AllInOneUglyGodAPI/settings.py
+++ AllInOneUglyGodAPI/AllInOneUglyGodAPI/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
